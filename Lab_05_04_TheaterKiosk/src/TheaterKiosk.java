465bb15795a8a041cf1a6b98b9641a9de780bab5
Wayassou <wayassou@gmail.com>
2024-10-29 22:45:16 -0400

    Add files via upload

---
+++ Lab_05_04_TheaterKiosk/src/TheaterKiosk.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class TheaterKiosk {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        if (age >= 21) {
+            System.out.println("You get a wristband.");
+        }
+    }
+}
